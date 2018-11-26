935e5bac292724a268734f4f2c33e3f50c5dad93
Dave Mason <DMasonSQL@GMail.com>
2018-11-25 21:40:00 -0500

    Update Run SQLtoExcel.bat

--- Run SQLtoExcel.bat
+++ Run SQLtoExcel.bat
@@ -9,7 +9,7 @@ REM NOTE! If /ScriptsFolder ends with a backslash, add a space character afterwa
 REM https://stackoverflow.com/questions/1291291/how-to-accept-command-line-args-ending-in-backslash#1291306
 
 REM SQL Authentication
-REM SQLtoExcel.exe /Server=.\SQL2017 /Database=master /Login=sa /Password=password /ScriptsFolder="%~dp0 " /ExcelFile="%~dp0Addendum.xlsx"
+REM SQLtoExcel.exe /Server=.\SQL2017 /Database=master /Login=sa /Password=password /ScriptsFolder="%~dp0 " /ExcelFile="%~dp0SQLtoExcel.xlsx"
 
 REM Windows Authentication (login/pwd omitted)
 SQLtoExcel.exe /Server=.\SQL2017 /Database=master /ScriptsFolder="%~dp0 " /ExcelFile="%~dp0SQLtoExcel.xlsx"
