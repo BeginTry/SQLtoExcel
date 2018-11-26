d4bc12395f6665f082e06753d8fe0cf5c54c9496
Dave Mason <DMasonSQL@GMail.com>
2018-11-25 21:35:53 -0500

    Update Run SQLtoExcel.bat

--- Run SQLtoExcel.bat
+++ Run SQLtoExcel.bat
@@ -5,9 +5,6 @@ CLS
 
 CD %~dp0
 
-REM ECHO "%~dp0"
-REM ECHO "%~dp0Addendum.xlsx"
-
 REM NOTE! If /ScriptsFolder ends with a backslash, add a space character afterwards.
 REM https://stackoverflow.com/questions/1291291/how-to-accept-command-line-args-ending-in-backslash#1291306
 
